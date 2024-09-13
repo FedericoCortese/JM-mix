356e9d38eb4d76cc72fc5019522b575e85240f72
Federico P. Cortese <67327161+FedericoCortese@users.noreply.github.com>
2024-09-13 15:40:12 +0200

    updates

--- example.R
+++ example.R
@@ -1,15 +1,15 @@
 source("Utils_JMmix.R")
 
 # Simulate mixed data with 10% continuous missing values
-TT=1000
-P=30 
-Pcat=15
-Ktrue=3
-mu=2
-phi=.8
-rho=0
-pers=.9
-pNAs=.1 
+TT=1000 # Number of time steps
+P=30 # Number of features
+Pcat=15 # Number of categorical features
+Ktrue=3 # Number of true states
+mu=2 # Mean value for the continuous variable 
+phi=.8 # Conditional probability for the categorical outcome k in state k
+rho=0 # Correlation
+pers=.9 # Persistence
+pNAs=.1 # Percentage of missing values
 typeNA=1 # 0 for random missing, 1 for continuous missing
 
 Y=sim_data_mixed(seed=1,
@@ -24,7 +24,7 @@ Y=sim_data_mixed(seed=1,
          pNAs=pNAs,
          typeNA=typeNA)
 
-# Estimate
+# Estimation
 lambda=.16
 
 est=jump_mixed(Y$SimData.NA,
